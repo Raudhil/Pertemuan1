c3326d72701f31bd521353cf353f58ef82093cd7
Raudhil <raudhilfirdausn@gmail.com>
2023-11-01 20:08:27 +0700

    Perkenalan Identitas

--- lolol.java
+++ lolol.java
@@ -1,32 +1,9 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class lolol{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
         
-        int [] a = new int [3];
-
-        a[0] = 1;
-        a[1] = 2;
-        a[2] = 3;
-
-        for(int i = 0; i < a.length; i++){
-            System.out.println(a[i]);
-        }
-
-        for(int j = 0; j < a.length; j++){
-            a[j] = j + 4;
-        }
-
-        System.out.println(" ");
-
-        for(int i = 0; i < a.length; i++){
-            System.out.println(a[i]);
-        }
-
-        FileOutputStream file1 = new FileOutputStream("input.txt");
-        FileInputStream file = new FileInputStream("input.txt");
-    
+        System.out.println("Halo nama saya Raudhil Firdaus Naufal");
+        System.out.println("Saya Mahasiswa di Polinema");
+        System.out.println("Dari jurusan TI");
     }
 }
