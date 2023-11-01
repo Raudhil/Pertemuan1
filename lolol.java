b2ae63d6fc799e9723157bebc92b5bb8a524d30a
Raudhil <raudhilfirdausn@gmail.com>
2023-11-01 19:44:19 +0700

    Perkenalan

---
+++ lolol.java
@@ -0,0 +1,14 @@
+public class lolol{
+    public static void main(String[] args) {
+        
+        int [] a = new int [3];
+
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
